787672fff9d16282481209829df84b505dc0c74e
duongsang97 <duongsang97@gmail.com>
2022-08-05 00:16:18 +0700

    update

--- api/controllers/tickets/ticketController.js
+++ api/controllers/tickets/ticketController.js
@@ -35,7 +35,7 @@ function TicketController() {
                                 $or:[{"updatedAt":dateQuery},{"appointmentTime":dateQuery}]
                             },
                             {
-                                "recordStatus":1, "hostId":hostId,
+                                "recordStatus":1, "hostId":hostId,"ofGarage":garageSelected
                             }
                             ]
                         }
